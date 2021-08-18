41dd16c88384361c60d090e35b56c2762a031b62
dvrevb <dvrevb@outlook.com>
2021-08-19 01:49:19 +0300

    update json2mask

--- src/json2mask.py
+++ src/json2mask.py
@@ -1,26 +1,15 @@
+#import libraries
 import json
 import os
 import numpy as np
 import cv2
 import tqdm
-from constant import JSON_DIR, MASK_DIR
-
-
+from constant import JSON_DIR, MASK_DIR #constant values
 
 # Create a list which contains every file name in "jsons" folder
 json_list = os.listdir(JSON_DIR)
 
-""" tqdm Example Start"""
-
-iterator_example = range(1000000)
-
-for i in tqdm.tqdm(iterator_example):
-    pass
-
-""" rqdm Example End"""
-
-
-# For every json file
+# For every json file   # tqdm used for smart progress bar
 for json_name in tqdm.tqdm(json_list):
 
     # Access and open json file as dictionary
@@ -31,7 +20,9 @@ for json_name in tqdm.tqdm(json_list):
     json_dict = json.load(json_file)
 
     # Create an empty mask whose size is the same as the original image's size
+    #0'lardan olusan bir matris olusturuyoruz. resim boyutları annotate edilmiş resimlerden geliyor
     mask = np.zeros((json_dict["size"]["height"], json_dict["size"]["width"]), dtype=np.uint8)
+    #olusan maske nereye kaydedilecek
     mask_path = os.path.join(MASK_DIR, json_name[:-9]+".png")
 
     # For every objects
